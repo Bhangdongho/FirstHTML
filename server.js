5fb0391f5b26dcbfabc7b4e7c109c217101a6e43
Bhangdongho <bhang97@naver.com>
2024-04-18 07:39:57 +0900

    index.js router.js 추가

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 let http = require("http");
+let url = require("url");
 
-function onRequest(request, response) {
-  response.writeHead(200, { "content-Type": "text/html" });
-  response.write("Hello Node.js");
-  response.end();
+function start(route) {
+  function onRequest(request, response) {
+    let pathname = url.parse(request.url).pathname;
+    route(pathname);
+
+    response.writeHead(200, { "content-Type": "text/html" });
+    response.write("Hello Node.js");
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  // localhost:8888
 }
 
-http.createServer(onRequest).listen(8888);
+exports.start = start;
